e95f813aec700ebe9031f6990e9675c9c2bd9044
Igor Paiva <igor_paiva21@live.com>
2023-09-22 15:32:25 -0300

    Adding register page

--- src/app/public/components/register/register.component.ts
+++ src/app/public/components/register/register.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { AuthService } from './../../services/auth.service';
+import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { CustomValidators } from '../../custom-validator';
+import { tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +11,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+  registerForm = new UntypedFormGroup({
+    email: new UntypedFormControl(null, [Validators.required, Validators.email]),
+    login: new UntypedFormControl(null, [Validators.required]),
+    nome: new UntypedFormControl(null, [Validators.required]),
+    sobrenome: new UntypedFormControl(null, [Validators.required]),
+    password: new UntypedFormControl(null, [Validators.required]),
+    passwordConfirm: new UntypedFormControl(null, [Validators.required])
+  },
+    // add custom Validators to the form, to make sure that password and passwordConfirm are equal
+    { validators: CustomValidators.passwordsMatching }
+  )
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  register() {
+    if (!this.registerForm.valid) {
+      return;
+    }
+    this.authService.register(this.registerForm.value).pipe(
+      // If registration was successfull, then navigate to login route
+      tap(() => this.router.navigate(['../login']))
+    ).subscribe();
+  }
 
 }
